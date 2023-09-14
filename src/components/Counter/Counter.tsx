e2eb0957a4c18feed996979f94fda66622125479
Sergei Bakutov <bakutenok@MacBook-Pro-Sergei.local>
2023-09-14 15:45:10 +0300

    chore: Добавление поддержки css

---
+++ src/components/Counter/Counter.tsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+
+import './Counter.scss'
+
+export const Counter: React.FC = () => {
+  const [count, setCount] = useState(0)
+
+  const increment = () => {
+    setCount(prev => prev + 1)
+  }
+
+  return (
+    <div>
+      <h1>{count}</h1>
+      <button onClick={increment}>increment</button>
+    </div>
+  )
+}
--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,6 @@
 import { createRoot } from 'react-dom/client'
 
+import { Counter } from './components/Counter/Counter'
+
 const root = createRoot(document.getElementById('root') as HTMLElement)
-root.render(<h1>Hello, world!!!</h1>)
+root.render(<Counter />)
