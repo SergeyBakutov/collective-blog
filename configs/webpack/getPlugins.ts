0c5233137ae3c9cb57f3f9ba35812c3786f9e44d
Sergei Bakutov <bakutenok@MacBook-Pro-Sergei.local>
2023-09-21 10:46:35 +0300

    build: Добавление BundleAnalyzer

--- configs/webpack/getPlugins.ts
+++ configs/webpack/getPlugins.ts
@@ -2,6 +2,7 @@ import webpack from 'webpack'
 import HtmlWebpackPlugin from 'html-webpack-plugin'
 import MiniCssExtractPlugin from 'mini-css-extract-plugin'
 import ReactRefreshWebpackPlugin from '@pmmmwh/react-refresh-webpack-plugin'
+import { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer'
 
 interface TPluginsOptions {
   htmlTemplatePath: string
@@ -19,6 +20,9 @@ export function getPlugins(options: TPluginsOptions): webpack.WebpackPluginInsta
     }),
     new webpack.DefinePlugin({
       __IS_DEV__: isDev
+    }),
+    new BundleAnalyzerPlugin({
+      openAnalyzer: false
     })
   ]
 
