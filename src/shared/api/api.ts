2cb8c1e45f6ea8cdbaa80b62f1948c5de42360b4
Sergei Bakutov <bakutenok@MacBook-Pro-Sergei.local>
2023-11-13 19:55:09 +0300

    fix: Исправления в api

--- src/shared/api/api.ts
+++ src/shared/api/api.ts
@@ -3,8 +3,11 @@ import axios from 'axios'
 import { LOCAL_STORAGE_USER_KEY } from 'shared/constants/localStorage'
 
 export const $api = axios.create({
-  baseURL: __API__,
-  headers: {
-    authorization: localStorage.getItem(LOCAL_STORAGE_USER_KEY)
-  }
+  baseURL: __API__
+})
+
+$api.interceptors.request.use((config) => {
+  config.headers.authorization = localStorage.getItem(LOCAL_STORAGE_USER_KEY) ?? ''
+
+  return config
 })
